83321073cb32524e3ea76564def75a7851018435
ThunderSly <brunex887@gmail.com>
2019-04-08 16:21:12 -0300

    Implementado com interruptions

--- SAME70_FONTS/SAME70_FONTS/src/main.c
+++ SAME70_FONTS/SAME70_FONTS/src/main.c
@@ -10,22 +10,66 @@
 #include "sourcecodepro_28.h"
 #include "calibri_36.h"
 #include "arial_72.h"
+#include "math.h"
 
-#define YEAR        2018
-#define MOUNTH      3
-#define DAY         19
-#define WEEK        12
-#define HOUR        15
-#define MINUTE      45
+
+#define YEAR        2019
+#define MONTH		4
+#define DAY         8
+#define WEEK        15
+#define HOUR        16
+#define MINUTE      18
 #define SECOND      0
 
-#define BUT_PIO				  PIOA
-#define BUT_PIO_ID			  ID_PIOA
-#define BUT_PIO_IDX			  19
-#define BUT_PIO_IDX_MASK	  (1 << BUT_PIO_IDX)
-#define BUT_DEBOUNCING_VALUE  79
+#define BUT3_PIO				PIOA
+#define BUT3_PIO_ID				ID_PIOA
+#define BUT3_PIO_IDX			19
+#define BUT3_PIO_IDX_MASK		(1 << BUT3_PIO_IDX)
+#define BUT3_DEBOUNCING_VALUE   79
 
 struct ili9488_opt_t g_ili9488_display_opt;
+
+volatile uint16_t counter_total = 0;
+volatile uint16_t counter_temp = 0;
+
+volatile uint8_t flag_but3 = 0;
+volatile uint8_t flag_rtt = 1;
+volatile uint8_t flag_rtc = 1;
+
+static float raio = 0.325;
+
+volatile uint32_t hour;
+volatile uint32_t minute;
+volatile uint32_t second;
+
+
+
+void RTC_Handler(void)
+{
+	uint32_t ul_status = rtc_get_status(RTC);
+
+	/*
+	*  Verifica por qual motivo entrou
+	*  na interrupcao, se foi por segundo
+	*  ou Alarm
+	*/
+	if ((ul_status & RTC_SR_SEC) == RTC_SR_SEC) {
+		rtc_clear_status(RTC, RTC_SCCR_SECCLR);
+		flag_rtc = 1;
+	}
+	
+	/* Time or date alarm */
+	if ((ul_status & RTC_SR_ALARM) == RTC_SR_ALARM) {
+		rtc_clear_status(RTC, RTC_SCCR_ALRCLR);
+	}
+	
+	
+	rtc_clear_status(RTC, RTC_SCCR_ACKCLR);
+	rtc_clear_status(RTC, RTC_SCCR_TIMCLR);
+	rtc_clear_status(RTC, RTC_SCCR_CALCLR);
+	rtc_clear_status(RTC, RTC_SCCR_TDERRCLR);
+	
+}
 
 void RTT_Handler(void)
 {
@@ -39,17 +83,26 @@ void RTT_Handler(void)
 
 	/* IRQ due to Alarm */
 	if ((ul_status & RTT_SR_ALMS) == RTT_SR_ALMS) {
-		pin_toggle(LED_PIO, LED_IDX_MASK);    // BLINK Led
-		f_rtt_alarme = true;                  // flag RTT alarme
+		flag_rtt = 1;
 	}
 }
 
-void pin_toggle(Pio *pio, uint32_t mask){
-	if(pio_get_output_data_status(pio, mask))
-	pio_clear(pio, mask);
-	else
-	pio_set(pio,mask);
+void Button3_Handler(uint32_t id, uint32_t mask)
+{
+	counter_temp+=1;
+	counter_total+=1;
 }
+
+void BUT3_init(void){
+	pmc_enable_periph_clk(BUT3_PIO_ID);
+	pio_set_input(BUT3_PIO, BUT3_PIO_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+
+	pio_enable_interrupt(BUT3_PIO, BUT3_PIO_IDX_MASK);
+	pio_handler_set(BUT3_PIO, BUT3_PIO_ID, BUT3_PIO_IDX_MASK, PIO_IT_FALL_EDGE, Button3_Handler);
+	
+	NVIC_EnableIRQ(BUT3_PIO_ID);
+	NVIC_SetPriority(BUT3_PIO_ID, 1);
+};
 
 static void RTT_init(uint16_t pllPreScale, uint32_t IrqNPulses)
 {
@@ -70,6 +123,28 @@ static void RTT_init(uint16_t pllPreScale, uint32_t IrqNPulses)
 	NVIC_SetPriority(RTT_IRQn, 0);
 	NVIC_EnableIRQ(RTT_IRQn);
 	rtt_enable_interrupt(RTT, RTT_MR_ALMIEN);
+}
+
+void RTC_init(){
+	/* Configura o PMC */
+	pmc_enable_periph_clk(ID_RTC);
+
+	/* Default RTC configuration, 24-hour mode */
+	rtc_set_hour_mode(RTC, 0);
+
+	/* Configura data e hora manualmente */
+	rtc_set_date(RTC, YEAR, MONTH, DAY, WEEK);
+	rtc_set_time(RTC, HOUR, MINUTE, SECOND);
+
+	/* Configure RTC interrupts */
+	NVIC_DisableIRQ(RTC_IRQn);
+	NVIC_ClearPendingIRQ(RTC_IRQn);
+	NVIC_SetPriority(RTC_IRQn, 0);
+	NVIC_EnableIRQ(RTC_IRQn);
+
+	/* Ativa interrupcao via alarme */
+	rtc_enable_interrupt(RTC,  RTC_IER_SECEN);
+
 }
 
 void configure_lcd(void){
@@ -105,11 +180,35 @@ int main(void) {
 	board_init();
 	sysclk_init();	
 	configure_lcd();
+	WDT->WDT_MR = WDT_MR_WDDIS;
+	uint16_t pllPreScale = (int) (((float) 32768) / 2.0);
+	uint32_t irqRTTvalue  = 8;
+	BUT3_init();
+	RTC_init();
 	
-	font_draw_text(&sourcecodepro_28, "OIMUNDO", 50, 50, 1);
-	font_draw_text(&calibri_36, "Oi Mundo! #$!@", 50, 100, 1);
-	font_draw_text(&arial_72, "102456", 50, 200, 2);
+
 	while(1) {
+		if (flag_rtt){
+			RTT_init(pllPreScale, irqRTTvalue);
+			float vel_ang = 2*PI*counter_temp/4;
+			float vel_lin = vel_ang * raio;
+			float distancia = 2*PI*raio*counter_total;
+			char b[512];
+			char b2[512];
+			sprintf(b,"Velocidade: %lf",vel_lin);
+			sprintf(b2,"Distância: %lf",distancia);
+			font_draw_text(&calibri_36, b, 50, 50, 1);
+			font_draw_text(&calibri_36, b2, 50, 100, 1);
+			counter_temp = 0;
+			flag_rtt = 0;
+		}
+		if (flag_rtc){
+			rtc_get_time(RTC,&hour,&minute,&second);
+			char b3[512];
+			sprintf(b3,"Horas: %d, Minutos: %d, Segundos: %d",hour,minute,second)
+			font_draw_text(&calibri_36, b3, 50, 150, 1);
+		}
+		pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
 		
 	}
 }
